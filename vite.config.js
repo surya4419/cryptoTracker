eac6d6c17d8e6250619fe8464b4974bd861f972d
suryaa4419@gmail.com <surya4419@gmail.com>
2025-01-28 15:46:43 +0530

    updated packages.json

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Make sure the output directory is set to 'build'
+  },
 })
